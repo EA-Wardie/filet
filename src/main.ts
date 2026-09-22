09b4f2ce62e38f13e9f63482c7fe59af4a56aa02
Edward van Tonder <eawardie.vt@gmail.com>
2026-09-22 20:03:58 +0200

    Removed unused code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { App } from "./components/App";
-// import { Column } from "./components/Column";
 import { Explorer } from "./components/Explorer";
 import { Flex } from "./components/Flex";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-// import { Toolbar } from "./components/Toolbar";
 import { makeApp } from "./lib/context";
 
 function main() {
@@ -16,21 +14,12 @@ function main() {
         Flex.make({
           components: [
             Header.make(),
-            // Toolbar.make().component,
             Explorer.make().component,
             Footer.make().component,
           ],
         }),
       ],
     });
-    // Layout.make().components([
-    // 	Sidebar.make(),
-    // 	Column.make().components([
-    // 		Toolbar.make(),
-    // 		Explorer.make(),
-    // 		Footer.make(),
-    // 	]),
-    // ]);
   });
 }
 
