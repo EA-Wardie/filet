33db7530328d9ca803121defa37cc41d9ab7db99
Edward van Tonder <eawardie.vt@gmail.com>
2026-09-22 20:03:04 +0200

    Rewrite for app, flex & header

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,37 @@
-import { Column } from "./components/Column";
+import { App } from "./components/App";
+// import { Column } from "./components/Column";
 import { Explorer } from "./components/Explorer";
+import { Flex } from "./components/Flex";
 import { Footer } from "./components/Footer";
-import { Layout } from "./components/Layout";
+import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Toolbar } from "./components/Toolbar";
+// import { Toolbar } from "./components/Toolbar";
 import { makeApp } from "./lib/context";
 
 function main() {
-	makeApp((): void => {
-		Layout.make().components([
-			Sidebar.make(),
-			Column.make().components([
-				Toolbar.make(),
-				Explorer.make(),
-				Footer.make(),
-			]),
-		]);
-	});
+  makeApp((): void => {
+    App.make({
+      components: [
+        Sidebar.make().component,
+        Flex.make({
+          components: [
+            Header.make(),
+            // Toolbar.make().component,
+            Explorer.make().component,
+            Footer.make().component,
+          ],
+        }),
+      ],
+    });
+    // Layout.make().components([
+    // 	Sidebar.make(),
+    // 	Column.make().components([
+    // 		Toolbar.make(),
+    // 		Explorer.make(),
+    // 		Footer.make(),
+    // 	]),
+    // ]);
+  });
 }
 
 main();
